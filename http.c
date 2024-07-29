b8b80779b8d456148899ec87cd7bf2d3c34a5a80
Sam Grantham <sagran@microsoft.com>
2024-07-29 14:10:34 -0500

    update

--- http.c
+++ http.c
@@ -1114,6 +1114,7 @@ static CURL *get_curl_handle(void)
 	if (getenv("GIT_SSL_CIPHER_LIST"))
 		ssl_cipherlist = getenv("GIT_SSL_CIPHER_LIST");
 	if (ssl_cipherlist != NULL && *ssl_cipherlist)
+	{
 		if ((ARRAY_SIZE(sslversions) == 1) && (strcmp("tlsv1.3", sslversions[0].name)))
 		{	
 			curl_easy_setopt(result, CURLOPT_TLS13_CIPHERS,	ssl_cipherlist);
@@ -1122,6 +1123,7 @@ static CURL *get_curl_handle(void)
 		{	
 			curl_easy_setopt(result, CURLOPT_SSL_CIPHER_LIST, ssl_cipherlist);
 		}
+	}
 	if (ssl_cert)
 		curl_easy_setopt(result, CURLOPT_SSLCERT, ssl_cert);
 	if (ssl_cert_type)
